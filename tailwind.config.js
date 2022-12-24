63cd43f8f7fd71e5d310a0cb07cf8d58d01e2aaa
arrlancores <arrlancore@gmail.com>
2022-12-24 15:35:52 +0700

    set new color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,22 @@ module.exports = {
     extend: {
       colors: {
         primary: "#01959F",
+        secondary: "#FA9810",
+        danger: "#E11428",
+        success: "#43936C",
+
+        "default-black": "#404040",
+        "strong-black": "#333333",
+
+        "primary-light": "#4DB5BC",
+        "secondary-light": "#FEEABC",
+        "danger-light": "#F5B1B7",
+        "success-light": "#B8DBCA",
+
+        "primary-surface": "#F7FEFF",
+        "secondary-surface": "#FFFCF5",
+        "danger-surface": "#FFFAFA",
+        "success-surface": "#F8FBF9",
       },
       fontFamily: {
         sans: ["Nunito Sans", "sans-serif"],
