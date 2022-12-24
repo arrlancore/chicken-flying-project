c6743d6ff96a769a1ecd809ef524469b8d161f1c
arrlancores <arrlancore@gmail.com>
2022-12-24 23:09:19 +0700

    fix modal styling

--- src/base-components/Modal.js
+++ src/base-components/Modal.js
@@ -13,7 +13,7 @@ const Modal = ({ open, onClose, title, content, footer }) => {
       <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
       <div className="fixed inset-0 z-10 overflow-y-auto">
         <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-          <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+          <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-[420px]">
             <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
               <div className="flex justify-between">
                 <h3 className="font-bold text-lg text-[#1D1F20]">{title}</h3>
@@ -27,9 +27,7 @@ const Modal = ({ open, onClose, title, content, footer }) => {
             {/* content */}
             <div className="px-6 pb-4">{content}</div>
             {/* footer */}
-            <div className="px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6 gap-3">
-              {footer}
-            </div>
+            <div className="px-4 py-3 flex sm:px-6">{footer}</div>
           </div>
         </div>
       </div>
