f12f31eb6bbcb43b811bd28a0eb5d04e52de6822
arrlancores <arrlancore@gmail.com>
2022-12-25 15:03:43 +0700

    add hide menu condition

--- src/project-roadmap/components/MenuItem.js
+++ src/project-roadmap/components/MenuItem.js
@@ -4,24 +4,26 @@ import { ReactComponent as ArrowRightIcon } from "../../assets/icon/arrow-right.
 import { ReactComponent as EditIcon } from "../../assets/icon/edit-alt.svg";
 import { ReactComponent as DeleteIcon } from "../../assets/icon/trash-alt.svg";
 
-const MenuItem = () => {
+const MenuItem = ({ hideLeft, hideRight }) => {
   const menus = [
-    { title: "Move Left", icon: <ArrowLeftIcon />, hide: false },
-    { title: "Move Right", icon: <ArrowRightIcon />, hide: false },
+    { title: "Move Left", icon: <ArrowLeftIcon />, hide: hideLeft },
+    { title: "Move Right", icon: <ArrowRightIcon />, hide: hideRight },
     { title: "Edit", icon: <EditIcon /> },
     { title: "Delete", icon: <DeleteIcon /> },
   ];
 
   return (
     <div className="flex p-4 gap-4 flex-col rounded-lg bg-white min-w-[260px] shadow-lg">
-      {menus.map((menu) => (
-        <div className="flex items-center cursor-pointer card-menu-item">
-          {menu.icon}
-          <div className="text-sm ml-4 text-strong-black font-[600]">
-            {menu.title}
+      {menus.map((menu) =>
+        menu.hide ? null : (
+          <div className="flex items-center cursor-pointer card-menu-item">
+            {menu.icon}
+            <div className="text-sm ml-4 text-strong-black font-[600]">
+              {menu.title}
+            </div>
           </div>
-        </div>
-      ))}
+        )
+      )}
     </div>
   );
 };
--- src/project-roadmap/components/ProjectRoadmapMain.js
+++ src/project-roadmap/components/ProjectRoadmapMain.js
@@ -1,5 +1,6 @@
 import { ReactComponent as CheckList } from "../../assets/icon/checklist.svg";
 import { ReactComponent as MoreHorizontalIcon } from "../../assets/icon/more-horizontal.svg";
+import { ReactComponent as PlusCircleIcon } from "../../assets/icon/plus-circle.svg";
 import GroupCard from "./GroupCard";
 import GroupLabel from "./GroupLabel";
 import ItemCard from "./ItemCard";
@@ -25,7 +26,12 @@ const toVariant = (index) => {
   }
 };
 
-const ProjectRoadmapMain = () => {
+const caption = {
+  noTask: "No Task",
+  createNewTask: "Create New Task",
+};
+
+const ProjectRoadmapMain = ({ onCreateNewTask, onEditTask }) => {
   const todo = useSelector((state) => state.todo);
   const user = useSelector((state) => state.user);
   const dispatch = useDispatch();
@@ -40,46 +46,79 @@ const ProjectRoadmapMain = () => {
   return renderIf(user.isLoggedIn)(
     <section id="form" className="mt-5">
       <div className="flex items-start flex-wrap gap-4 overflow-x-auto">
-        {todo.todos?.map((data, index) => (
-          <div key={index} className="mb-4 flex-1 max-w-[326px] min-w-[326px]">
-            <GroupCard variant={toVariant(index)}>
-              <GroupLabel variant={toVariant(index)}>{data.title}</GroupLabel>
+        {todo.todos?.map((data, indexTodo) => (
+          <div
+            key={indexTodo}
+            className="mb-4 flex-1 max-w-[326px] min-w-[326px]"
+          >
+            <GroupCard variant={toVariant(indexTodo)}>
+              <GroupLabel variant={toVariant(indexTodo)}>
+                {data.title}
+              </GroupLabel>
               <div className="h-3" />
               <div className="text-xs leading-5 font-bold">
                 {data.description}
               </div>
-              {data.items?.map((item, index) => (
-                <Fragment key={index}>
+              {data.items.length ? (
+                data.items.map((item, index) => (
+                  <Fragment key={index}>
+                    <div className="h-3" />
+                    <ItemCard>
+                      <div className="text-sm leading-6 font-bold">
+                        {item.name}
+                      </div>
+                      <div className="border-b-[#E0E0E0] border-b-[1px] border-dashed my-1" />
+                      <div className="flex justify-between">
+                        <div className="flex items-center">
+                          <Progress percentage={item.progress_percentage} />
+                          <div className="ml-2">
+                            {item.progress_percentage === 100 ? (
+                              <CheckList />
+                            ) : (
+                              <span className="text-xs text-[#75757]">
+                                {item.progress_percentage + "%"}
+                              </span>
+                            )}
+                          </div>
+                        </div>
+
+                        <PopoverCustom
+                          content={
+                            <MenuItem
+                              hideLeft={indexTodo === 0}
+                              hideRight={indexTodo === todo.todos.length - 1}
+                            />
+                          }
+                        >
+                          <MoreHorizontalIcon
+                            id={PopoverCustom.id}
+                            className="hover:bg-[#EDEDED] hover:rounded cursor-pointer"
+                          />
+                        </PopoverCustom>
+                      </div>
+                    </ItemCard>
+                  </Fragment>
+                ))
+              ) : (
+                <>
                   <div className="h-3" />
                   <ItemCard>
-                    <div className="text-sm leading-6 font-bold">
-                      {item.name}
-                    </div>
-                    <div className="border-b-[#E0E0E0] border-b-[1px] border-dashed my-1" />
-                    <div className="flex justify-between">
-                      <div className="flex items-center">
-                        <Progress percentage={item.progress_percentage} />
-                        <div className="ml-2">
-                          {item.progress_percentage === 100 ? (
-                            <CheckList />
-                          ) : (
-                            <span className="text-xs text-[#75757]">
-                              {item.progress_percentage + "%"}
-                            </span>
-                          )}
-                        </div>
-                      </div>
-
-                      <PopoverCustom content={<MenuItem />}>
-                        <MoreHorizontalIcon
-                          id={PopoverCustom.id}
-                          className="hover:bg-[#EDEDED] hover:rounded cursor-pointer"
-                        />
-                      </PopoverCustom>
-                    </div>
+                    <span className="text-sm leading-6 text-[#757575]">
+                      {caption.noTask}
+                    </span>
                   </ItemCard>
-                </Fragment>
-              ))}
+                  <div className="h-3" />
+                  <div
+                    className="flex cursor-pointer"
+                    onClick={() => onCreateNewTask(data.id)}
+                  >
+                    <PlusCircleIcon />
+                    <div className="text-xs leading-5 ml-2">
+                      {caption.createNewTask}
+                    </div>
+                  </div>
+                </>
+              )}
             </GroupCard>
           </div>
         ))}
