cb86e2fa8979707f03dc17af278db81fb8c675d3
arrlancores <arrlancore@gmail.com>
2022-12-25 07:43:10 +0700

    add url constant

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+export const BASE_URL = "https://todo-api-18-140-52-65.rakamin.com";
+
+export const LOGIN_URL = `${BASE_URL}/auth/login`;
+export const SIGNUP_URL = `${BASE_URL}/auth/signup`;
+export const TODO_URL = `${BASE_URL}/todos`;
+export const ITEM_URL = `${BASE_URL}/todos/{id}/items`;
+export const PATCH_ITEM_URL = `${BASE_URL}/todos/{id}/items/{targetTodoId}`;
