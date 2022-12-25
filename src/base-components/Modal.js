aa0ccf02bf95b15a649da8f9676e1a2f5da8f43b
arrlancores <arrlancore@gmail.com>
2022-12-25 10:40:00 +0700

    fix modal opacity

--- src/base-components/Modal.js
+++ src/base-components/Modal.js
@@ -10,7 +10,7 @@ const Modal = ({ open, onClose, title, content, footer }) => {
       role="dialog"
       aria-modal="true"
     >
-      <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+      <div className="fixed inset-0 bg-slate-700 bg-opacity-30 transition-opacity" />
       <div className="fixed inset-0 z-10 overflow-y-auto">
         <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
           <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-[420px]">
