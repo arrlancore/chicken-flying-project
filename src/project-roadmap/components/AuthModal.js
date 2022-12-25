464961117a277286ccc64bc72794ff0a4a645e9d
arrlancores <arrlancore@gmail.com>
2022-12-25 18:18:15 +0700

    remove unused import

--- src/project-roadmap/components/AuthModal.js
+++ src/project-roadmap/components/AuthModal.js
@@ -1,4 +1,3 @@
-import { Action } from "@remix-run/router";
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Button from "../../base-components/Button";
