4fd4c8a118a2a1ad8455b7f872100c2e097e1931
arrlancores <arrlancore@gmail.com>
2022-12-25 08:19:47 +0700

    fix app route to v1

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,27 +1,31 @@
-import React from 'react'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import React, { useEffect } from "react";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
 
-import ProjectRoadmap from './project-roadmap'
+import ProjectRoadmap from "./project-roadmap";
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <div>Hello world!</div>,
+    path: "/",
+    element: <Navigate to="/v1" replace={true} />,
   },
   {
-    path: '/v1',
+    path: "/v1",
     element: <ProjectRoadmap />,
   },
   {
-    path: '*',
+    path: "*",
     element: (
       <h2 className=" block text-3xl font-bold text-center text-cyan-700">
         404 Notfound
       </h2>
     ),
   },
-])
+]);
 
 export default function Routes() {
-  return <RouterProvider router={router} />
+  return <RouterProvider router={router} />;
 }
